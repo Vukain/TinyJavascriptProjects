7a8532e4418b588b42af9c5982319f5cc128f3ad
Michal Pi <nilfenheim@gmail.com>
2020-10-07 00:06:21 +0200

    add build

---
+++ ReactSPA/build/precache-manifest.a5c59ba678d037ac193446823c9e6292.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7c1a3f5bd8aa5624a81b46f6d48209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfee3f76b49db4a0cc3f",
+    "url": "/static/css/main.84e8a0a4.chunk.css"
+  },
+  {
+    "revision": "62e05a298f7f6ecbdedb",
+    "url": "/static/js/2.28765ef9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28765ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfee3f76b49db4a0cc3f",
+    "url": "/static/js/main.73b8e88e.chunk.js"
+  },
+  {
+    "revision": "3a47a246c5017f35c0fc",
+    "url": "/static/js/runtime-main.fb967fe0.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/static/media/header3.d9d36b65.jpg"
+  }
+]);
---
+++ ReactToDoApp/build/precache-manifest.8a988a45275cd9e10aa6bf968230b003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6650b25c2e3baa902228d04f23cef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa1e7bc9aa6507ae3f8",
+    "url": "/static/css/main.2c59a5be.chunk.css"
+  },
+  {
+    "revision": "873fcd93297af5100eea",
+    "url": "/static/js/2.a102fc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a102fc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa1e7bc9aa6507ae3f8",
+    "url": "/static/js/main.2f436129.chunk.js"
+  },
+  {
+    "revision": "0e782ca27fbec140139c",
+    "url": "/static/js/runtime-main.44a9e506.js"
+  }
+]);
