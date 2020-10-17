67ac9c609cd29a9eeebd3ed6f9ffbc5f35f926e4
Michal Pi <nilfenheim@gmail.com>
2020-10-17 12:58:08 +0200

    feat: implement of app store and request helper

--- ReactUdamu/src/App.jsx
+++ ReactUdamu/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import StoreProvider from './store/StoreProviderstore';
 import './App.scss';
 
 const App = () => {
   return ( 
+    <StoreProvider>
     <header>Hello World</header>
+    </StoreProvider>
   );
 };
 
