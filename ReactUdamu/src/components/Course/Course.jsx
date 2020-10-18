d20892be16c9d05dea14030f2c6999e352940960
Michal Pi <nilfenheim@gmail.com>
2020-10-19 01:19:34 +0200

    feat: user courses

---
+++ ReactUdamu/src/components/Course/Course.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+
+import { default as CourseStyles } from './Course.module.scss';
+import request from '../../helpers/request';
+import { StoreContext } from '../../store/StoreProvider';
+import { useHistory } from 'react-router-dom';
+
+const style = bemCssModules(CourseStyles);
+
+const Course = ({ authors, id, isUserContext = false, img, price, title }) => {
+
+  const { user, setUser } = useContext(StoreContext);
+  const history = useHistory();
+
+  const allAuthors = authors.join(', ');
+  const isUserLogged = Boolean(user);
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch(
+        '/users',
+        {
+          login: user.login,
+          courseId: id,
+        }
+      );
+      if (status === 202) {
+        setUser(data.user);
+        history.push('/my-courses');
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
+  console.log(shouldBeBuyButtonVisible);
+  return (
+    <li>
+      <article className={style()}>
+        <h3 className={style('title')}>{title}</h3>
+        <img src={img} alt={title} className={style('image')} />
+        <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
+        <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBuyButtonVisible && <button onClick={handleOnClick}>Zakup ten kurs</button>}
+      </article>
+    </li>
+  );
+}
+
+export default Course;
