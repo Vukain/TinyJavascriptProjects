67e4d049ae447487678fa159a237c3d0ddec614b
Vukain <55598879+Vukain@users.noreply.github.com>
2022-05-09 17:55:40 +0200

    Update auth.jsx

--- React/react-burger-builder-func-hooks/src/store/actions/auth.jsx
+++ React/react-burger-builder-func-hooks/src/store/actions/auth.jsx
@@ -49,9 +49,9 @@ export const auth = (email, password, isSignup) => {
             returnSecureToken: true
         }
 
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC2L1_-0hhczRzg15y7-Tb8Toe-qi53APQ';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=XXXXXXX';
         if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC2L1_-0hhczRzg15y7-Tb8Toe-qi53APQ'
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=XXXXXXX'
         }
         axios.post(url, authData)
         .then(response => {
